0539a10554abf0daaba54c28a7e9195cb38599ca
chungkr123 <aaalll332@gmail.com>
2021-01-27 13:50:22 +0900

    t2

--- airline/urls.py
+++ airline/urls.py
@@ -18,6 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("flights/", include("flights.urls")),
-    path("users/", include("users.urls"))
+    path("flights/", include("flights.urls"))
 ]
